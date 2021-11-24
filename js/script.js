de99bf60dca3ee8b614bb496b6a0ba24ae0557cd
Khaair <khair.cse.iu@gmail.com>
2021-11-24 18:30:28 +0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-var noti = document.querySelector('h1');
-	var select = document.querySelector('.select');
-	var button = document.getElementsByTagName('button');
-	for(var but of button){
-		but.addEventListener('click', (e)=>{
-			var add = Number(noti.getAttribute('data-count') || 0);
-			noti.setAttribute('data-count', add +1);
-			noti.classList.add('zero')
-
-		
-		})
-	}
-	
+var element = document.getElementById("incrementtext");
+        var value = element.innerHTML;
+        ++value;
+        console.log(value);
+        document.getElementById("incrementtext").innerHTML = value;
