595e35e4655206c5feea108a70da40f67208c5ca
Khaair <khair.cse.iu@gmail.com>
2021-11-24 15:48:26 +0600

    Update

---
+++ js/product.js
@@ -0,0 +1,22 @@
+const products=[{
+    id:1,
+    name:"rasel",
+    price:47,
+
+}
+,
+{
+    id:2,
+    name:"rasel",
+    price:47,
+
+}
+,
+{
+    id:3,
+    name:"rasel",
+    price:47,
+
+},
+]
+export default products;
